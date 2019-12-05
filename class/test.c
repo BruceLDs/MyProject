7005d489a83a938064c70d9aca8371f5327d4f18
dev <dev@localhost.localdomain>
2019-12-05 20:33:58 +0800

    8

---
+++ class/test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+printf("Hello linux!\r");
+printf("Hello linux!\n");
+printf("Hello linux!\n");
+printf("Hello linux!\n");
+printf("Hello linux!\n");
+printf("Hello linux!\n");
+printf("Hello linux!\n");
+printf("Hello linux!\n");
+printf("Hello linux!\n");
+  return 0;
+}
